9763f8eaaf64d3e1cd3ab7cac085a3956540663d
鲲鹏 <zhouzongkun@cai-inc.com>
2018-01-16 20:15:10 +0800

    添加swagger来显示文档

---
+++ src/main/java/com/zzk/spring/boot/pojo/User.java
@@ -0,0 +1,37 @@
+package com.zzk.spring.boot.pojo;
+
+/**
+ * Created by zhouzongkun on 2018/1/16.
+ */
+public class User {
+
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
